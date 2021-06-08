813cddcc72384a55304b8872618f82a4479eee90
BoberKrzysztof <k.bober@onet.pl>
2021-06-08 10:26:45 +0200

    Upgrade

--- app/src/main/java/com/example/drivertestapp/MainActivity.kt
+++ app/src/main/java/com/example/drivertestapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.drivertestapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nextActivity()
+    }
+
+    private fun nextActivity() {
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
