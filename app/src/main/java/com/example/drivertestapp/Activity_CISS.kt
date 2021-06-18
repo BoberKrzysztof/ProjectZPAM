91b3b33f28ec403b5ea530652f6e3799baf5358f
Max-Hub-King <84686070+Max-Hub-King@users.noreply.github.com>
2021-06-18 22:24:27 +0200

    działający CISS test

--- app/src/main/java/com/example/drivertestapp/Activity_CISS.kt
+++ app/src/main/java/com/example/drivertestapp/Activity_CISS.kt
@@ -2,10 +2,84 @@ package com.example.drivertestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
 
 class Activity_CISS : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity__c_i_s_s)
+
+        val next = findViewById<Button>(R.id.CISS_next)
+        val answ1 = findViewById<RadioButton>(R.id.CISS1)
+        val answ2 = findViewById<RadioButton>(R.id.CISS2)
+        val answ3 = findViewById<RadioButton>(R.id.CISS3)
+        val answ4 = findViewById<RadioButton>(R.id.CISS4)
+        val answ5 = findViewById<RadioButton>(R.id.CISS5)
+        val question = findViewById<TextView>(R.id.question)
+
+        val question_list = arrayOf(
+            "1. I am better organized in stressful situations",
+            "2. I can manage my time better in a stressful situation",
+            "3. In a stressful situation, I like to be alone",
+            "4. In a stressful situation, I prefer to be among people",
+            "5. In stressful situations, I often explode with anger",
+            "6. I can focus in a stressful situation",
+            "7. In a stressful situation, I shut myself off",
+            "8. In a stressful situation, I blame myself for failures",
+            "9. In a stressful situation, I think of the better old days",
+            "10. Maintains a sober mind in stressful situations",
+            "11. I can make decisions in stressful situations",
+            "12. I eat more than usual in stressful situations",
+            "13. In a stressful situation, I can't sleep",
+            "14. In a stressful situation, I have health problems",
+            "15. In a stressful situation, I don't worry about",
+            "16. In a stressful situation, I avoid difficult topics",
+            "17. In a stressful situation, I reach out to friends for advice",
+            "18. I get depressed in stressful situations"
+        )
+        val answers_list = ArrayList<String>()
+
+        var i = 0
+
+
+        next.setOnClickListener{
+            if (answ1.isChecked){
+                answers_list.add(question_list[i] + "\n" + answ1.text)
+                i++
+                question.setText(question_list[i])
+                answ1.setChecked(false)
+            }
+            else if (answ2.isChecked){
+                answers_list.add(question_list[i] + "\n" + answ2.text)
+                i++
+                question.setText(question_list[i])
+                answ2.setChecked(false)
+            }
+            else if (answ3.isChecked){
+                answers_list.add(question_list[i] + "\n" + answ3.text)
+                i++
+                question.setText(question_list[i])
+                answ3.setChecked(false)
+            }
+            else if (answ4.isChecked){
+                answers_list.add(question_list[i] + "\n" + answ4.text)
+                i++
+                question.setText(question_list[i])
+                answ4.setChecked(false)
+            }
+            else if (answ5.isChecked){
+                answers_list.add(question_list[i] + "\n" + answ5.text)
+                i++
+                question.setText(question_list[i])
+                answ5.setChecked(false)
+            }
+            else{
+                Toast.makeText(this, "Please enter your answer", Toast.LENGTH_SHORT).show()
+            }
+
+            }
+        }
     }
-}
