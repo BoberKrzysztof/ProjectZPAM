9a80f88c5e44cfad40a7e91556e16de5c31c3095
Max-Hub-King <84686070+Max-Hub-King@users.noreply.github.com>
2021-07-05 00:11:57 +0200

    reaction test - funkcionalność zrobiona zostało przepisanie wyniku i zrobienia 3 prób

--- app/src/main/java/com/example/drivertestapp/Activity_reaction_test.kt
+++ app/src/main/java/com/example/drivertestapp/Activity_reaction_test.kt
@@ -2,31 +2,91 @@ package com.example.drivertestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.os.Handler
 import android.os.SystemClock
 import android.view.View
 import android.widget.Button
 import android.widget.Chronometer
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.math.roundToInt
 
 class Activity_reaction_test : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reaction_test)
+
         val startButton = findViewById<Button>(R.id.start_button)
-        val stopButton = findViewById<Button>(R.id.circle_button)
-        val result = findViewById<TextView>(R.id.best_result)
+        val red = findViewById<Button>(R.id.red_button)
+        val green = findViewById<Button>(R.id.green_button)
+        green.setVisibility(View.GONE)
+        val best_result = findViewById<TextView>(R.id.best_result)
+        val average_result = findViewById<TextView>(R.id.average_result)
+        val time = findViewById<Chronometer>(R.id.time_reaction_test)
+        time.setVisibility(View.GONE)
+        val time_text = findViewById<TextView>(R.id.text_time)
+        var i = 0
+        var result_list = arrayOf<String>()
+
+        fun start () {
+            startButton.setOnClickListener {
+                i++
+                val delay = (1000..3000).random()
+                SystemClock.sleep(delay.toLong())
+                time.setBase(SystemClock.elapsedRealtime())
+                time.start()
+                red.setVisibility(View.GONE)
+                green.setVisibility(View.VISIBLE)
+                green.setOnClickListener {
+                    time.stop()
+                    val milisec = (SystemClock.elapsedRealtime() - time.getBase()).toString()
+                    time_text.setText(milisec)
+                    time_text.append(" milliseconds")
+                    green.setVisibility(View.GONE)
+                    red.setVisibility(View.VISIBLE)
+
+                    result_list += (milisec)
+
+                    var largest = result_list[0]
+                    for (ms in result_list) {
+                        if (largest.toInt() < ms.toInt()) {
+                            largest = ms
+                        }
+                    }
+
+                    best_result.setText(largest)
+                    best_result.append(" milliseconds")
+
+                    var average = 0
+                    for (ms in result_list) average += ms.toInt()
+                    average = average / result_list.size
 
 
-        startButton.setOnClickListener {
-            run()
+                    average_result.setText(average.toString())
+                    average_result.append(" milliseconds")
+                }
+            }
         }
 
-    }
+        start()
 
-    private fun run(){
+        /*val result = milisec[7].toString() + milisec[8].toString() + milisec[9].toString()
+        result_list += (result)
 
+        var largest = result_list[0]
+        for (ms in result_list) {
+            if (largest.toInt() < ms.toInt()) {
+                largest = ms
+            }
+        }
+
+        best_result.setText(largest)
+        best_result.append(" milliseconds")
+
+        var average: Double = 0.0
+        for (ms in result_list) average += ms.toDouble()
+        average = average / result_list.size
+
+        average_result.setText(average.toString())
+        average_result.append(" milliseconds")*/
     }
 }
